a6de5cce5f226fc55cbc341a153764941918ee6e
unknown <rahulkr.1531@gmail.com>
2023-05-20 20:07:38 +0530

    my commit in branch

--- src/test/java/com/firstPackage/myDemo.java
+++ src/test/java/com/firstPackage/myDemo.java
@@ -3,7 +3,7 @@ package com.firstPackage;
 public class myDemo {
 
 	public static void main(String[] args) {
-		System.out.println("kwqndok this will create conflict ");
+		System.out.println("kwqndok this may create conflict ");
 		
 		System.out.println("kwqmnb nmndok");
 		
