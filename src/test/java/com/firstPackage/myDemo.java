fd13838555e7395f4a0f14a5a8d602de9f09454c
unknown <rahulkr.1531@gmail.com>
2023-05-20 20:01:18 +0530

    my second commit in this project

--- src/test/java/com/firstPackage/myDemo.java
+++ src/test/java/com/firstPackage/myDemo.java
@@ -3,9 +3,11 @@ package com.firstPackage;
 public class myDemo {
 
 	public static void main(String[] args) {
-		System.out.println("kwqndok");
+		System.out.println("kwqndok this will create conflict ");
 		
 		System.out.println("kwqmnb nmndok");
+		
+		System.out.println("kwqmnb nmn mn ndok");
 		
 		System.out.println("kwqmnb nmn mn ndok");
 	}
