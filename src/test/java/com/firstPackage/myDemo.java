e64b37da3f3e115618e0204791ec7e03d9b7a658
unknown <rahulkr.1531@gmail.com>
2023-05-20 20:31:53 +0530

    my first commit in this project

--- src/test/java/com/firstPackage/myDemo.java
+++ src/test/java/com/firstPackage/myDemo.java
@@ -5,11 +5,9 @@ public class myDemo {
 	public static void main(String[] args) {
 		System.out.println("kwqndok this may create conflict ");
 		
-		System.out.println("kwqmnb nmndok");
+		System.out.println("kwqmnb nmnd nok");
 		
-		System.out.println("kwqmnb nmn mn ndok");
 		
-		System.out.println("kwqmnb nmn mn ndok");
 	}
 
 }
